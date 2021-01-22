a0490a061ef96c029837cafbe398a5ca446ac54d
Marlon Fernandez <marlonfsolis@outlook.com>
2021-01-21 21:59:55 -0500

    Display on Todo page a list of tasks.

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,11 +1,46 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Todo list</h1>
+  <q-page class="q-pa-md">
+    <q-list bordered>
+      <q-item v-ripple
+              clickable
+              v-for="task in tasks"
+              :key="task.id"
+              @click="task.completed = !task.completed"
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed"></q-checkbox>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{task.name}}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
+const taskList = [
+  {
+    id: 1,
+    name: 'Buy bananas',
+    completed: false
+  },
+  {
+    id: 2,
+    name: 'Buy beans',
+    completed: false
+  },
+  {
+    id: 3,
+    name: 'Buy apples',
+    completed: false
+  }
+]
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data: () => ({
+    tasks: taskList
+  })
 }
 </script>
